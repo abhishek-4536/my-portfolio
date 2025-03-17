c1b9e949f0cab791b858e31d144fbe16d16145f1
Abhishek <abhishek9872615721@gmail.com>
2025-03-18 00:42:30 +0530

    fixed sidebar issues

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,16 @@
 "use client";
 import Image from "next/image"
 import { useState } from "react";
+import { motion, Variants } from "framer-motion";
+
+const itemVariants: Variants = {
+    open: {
+        opacity: 1,
+        y: 0,
+        transition: { type: "spring", stiffness: 300, damping: 24 }
+    },
+    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } }
+};
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -8,6 +18,56 @@ const Navbar = () => {
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
+
+    type navbarProps = {
+        url: string;
+        name: string;
+    }
+
+    const navbarItems: navbarProps[] = [
+        {
+            url: "#home",
+            name: "Home"
+        },
+        {
+            url: "#about-me",
+            name: "About Me"
+        },
+        {
+            url: "#testimonials",
+            name: "Testimonials"
+        },
+        {
+            url: "experience",
+            name: "Experience"
+        },
+        {
+            url: "skills",
+            name: "Skills"
+        },
+        {
+            url: "#projects",
+            name: "Projects"
+        },
+        {
+            url: "#achievements",
+            name: "Achievements"
+        },
+        {
+            url: "#social-media",
+            name: "Social Media"
+        },
+        {
+            url: "#contact-me",
+            name: "Contact Me"
+        },
+        {
+            url: "#portfolio-made-with",
+            name: "Portfolio Made With"
+        }
+
+    ]
+
 
     return (
         <>
@@ -25,32 +85,65 @@ const Navbar = () => {
                             </div>
 
                             <div className="relative group">
-                                <div
+                                <motion.div
+                                    whileTap={{ scale: 0.97 }}
                                     className="flex items-center gap-6 text-white bg-black text-sm md:text-base xl:text-2xl lg:text-lg sm:pl-6 rounded-full h-full cursor-pointer"
                                     onClick={toggleMenu}
                                 >
                                     <p>Menu</p>
-                                    <div className="p-3 sm:p-4 cursor-pointer rounded-full bg-mainColor">
+                                    <motion.div
+                                        variants={{
+                                            open: { rotate: 180 },
+                                            closed: { rotate: 0 }
+                                        }}
+                                        transition={{ duration: 0.2 }}
+                                        style={{ originY: 0.55 }} className="p-3 sm:p-4 cursor-pointer rounded-full bg-mainColor">
                                         <Image src="/images/menu.svg" alt="menu" width={36} height={36} className="w-6 sm:w-6 lg:w-auto" />
-                                    </div>
-                                </div>
+                                    </motion.div>
+                                </motion.div>
 
-                                <div className={`absolute group-hover:block bg-white text-black flex-col gap-6 rounded-[10px] w-max top-full cursor-pointer right-0 flex-1 mt-2 text-sm font-normal truncate border-[1px] border-[#f1f1f1] ${isMenuOpen ? 'block' : 'hidden'}`}>
-                                    <ul>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#">Home</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#about-me">About Me</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#testimonials">Testimonials</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#education">Education</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#experience">Experience</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#skills">Skills</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#projects">Projects</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#achievements">Achievements</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#discuss-project">Discuss Project</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#social-media">Social Media</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#contact-me">Contact Me</a></li>
-                                        <li className="px-4 py-2 hover:bg-gray-100"><a href="#portfolio">Portfolio Made With?</a></li>
-                                    </ul>
-                                </div>
+                                <motion.div
+                                    animate={isMenuOpen ? "open" : "closed"}
+                                    initial="closed"
+                                    variants={{
+                                        open: {
+                                            opacity: 1,
+                                            height: "auto",
+                                            clipPath: "inset(0% 0% 0% 0% round 10px)",
+                                            transition: {
+                                                type: "spring",
+                                                bounce: 0,
+                                                duration: 0.7,
+                                                delayChildren: 0.3,
+                                                staggerChildren: 0.05
+                                            }
+                                        },
+                                        closed: {
+                                            opacity: 0,
+                                            height: 0,
+                                            clipPath: "inset(10% 50% 90% 50% round 10px)",
+                                            transition: {
+                                                type: "spring",
+                                                bounce: 0,
+                                                duration: 0.3
+                                            }
+                                        }
+                                    }}
+                                    className="absolute bg-black text-white flex-col gap-6 rounded-[10px] w-max top-full cursor-pointer right-0 flex-1 mt-2 text-sm font-normal truncate border-[1px] border-[#f1f1f1]"
+                                >
+                                    <motion.ul>
+                                        {navbarItems.map((item, index) => (
+                                            <motion.li
+                                                variants={itemVariants}
+                                                key={index}
+                                                className="px-4 pr-10 py-2 hover:bg-gray-100 hover:text-black"
+                                            >
+                                                <a href={item.url}>{item.name}</a>
+                                            </motion.li>
+                                        ))}
+                                    </motion.ul>
+                                </motion.div>
+
                             </div>
                         </div>
                     </div>
